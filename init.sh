da420aeffb51bb764fbc396af2509897fcf8ea71
mkalyakin <mkalyakin@tradingview.com>
2022-04-22 16:37:59 +0300

    fix dependency

--- init.sh
+++ init.sh
@@ -6,6 +6,6 @@ source ./source.sh "$1"
 "$1" -m pip install markupsafe==1.1.1
 "$1" -m pip install jinja2==2.10.1
 "$1" -m pip install sphinx==1.8.5
-"$1" -m pip install beautifulsoup4
+"$1" -m pip install bs4
 "$1" -m pip install lxml
 "$1" -m pip install doc8
