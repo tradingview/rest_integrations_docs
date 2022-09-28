bc1ceb8e8e46974d1061bc5ca228affdd30d1d39
Anton Osipov <45413939+qaosipov@users.noreply.github.com>
2022-09-28 11:39:43 +0300

    Update python

--- init.sh
+++ init.sh
@@ -1,11 +1,9 @@
 #!/bin/bash -e
-"$1" -m pip install --user virtualenv
-
 source ./source.sh "$1"
 
 "$1" -m pip install markupsafe==1.1.1
-"$1" -m pip install jinja2==2.10.1
-"$1" -m pip install sphinx==1.8.5
+"$1" -m pip install jinja2==3.1.2
+"$1" -m pip install sphinx==5.0.2
 "$1" -m pip install beautifulsoup4
 "$1" -m pip install lxml
 "$1" -m pip install doc8
